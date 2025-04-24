726bb4ed6a1f1fc7a57e244bc00808c534e060a7
christian <mcxespejo@tip.edu.ph>
2025-04-24 19:16:58 +0800

    Updated settings and added PostgreSQL support

--- core/settings.py
+++ core/settings.py
@@ -141,3 +141,31 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from pathlib import Path
+from dotenv import load_dotenv
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path( file ).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
+import os
+# core/settings.py
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+USER = config('USER')
+EMAIL = config('EMAIL')
+
+import dj_database_url
+
+# core/settings.py
+DATABASES = {
+'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
